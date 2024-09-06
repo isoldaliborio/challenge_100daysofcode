3ce9b3919441bd29708e2099efdd66976a0facea
isoldaliborio <isoldaliborio@gmail.com>
2024-09-06 21:22:51 +0100

    new solution for pangram

--- Python/34_chalenge_panagram_.py
+++ Python/34_chalenge_panagram_.py
@@ -2,38 +2,21 @@
 
 def pangrams(s):
     alphabet_set = set("abcdefghijklmnopqrstuvwxyz")
-    len_s = len(s)
-    result = None
-
-    for i in range(len_s):
-        letter = letter.lower()
-
-        for letter in alphabet_set:
-            if letter in s:
-                result = "pangram"
-            else:
-                result = "not pangram"
-    
-    return result
-
-s = "the quick brown fox jumps over the lazy dog"
-result = pangrams(s)
-print(result)
-
-# complexity o(n2)
-
-
+    s = set(s.lower())
+    if alphabet_set.issubset(s):
+        return "pangram"
+    else:
+        return "not pangram"
 
 
 
 # def pangrams(s):
 #     alphabet_set = set("abcdefghijklmnopqrstuvwxyz")
 #     len_s = len(s)
-#     for i in range(len_s):
-#         for letter in s:
-#             letter = letter.lower()
-#             if letter in alphabet_set:
-#                 alphabet_set.remove(letter)
+#     for letter in s:
+#         letter = letter.lower()
+#         if letter in alphabet_set:
+#             alphabet_set.remove(letter)
     
 #     if not alphabet_set:
 #         return "pangram"
